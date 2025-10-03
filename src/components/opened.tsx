f20474fa4a93d343fc07465687a0c213a5d5c41e
raj <raj.agarwal@remotestate.com>
2025-10-03 19:42:36 +0530

    built fixed

--- src/components/opened.tsx
+++ src/components/opened.tsx
@@ -78,6 +78,7 @@ export function OpenedSecton() {
 
     const restoreMutation = useMutation({
         mutationFn: async ({ foldName, foldId }: { foldName: string; foldId: string }) => {
+            console.log(foldName, foldId);
             const res = await axios.post(`${config.base_url}/notes/${fileId}/restore`);
             return res.data;
         },
